60ce325c034ded9bc1219124147eba4a8243a0da
Ichicodes <68382986+IxchelLopez@users.noreply.github.com>
2021-11-25 13:55:06 +0100

    Requerimiento 1

--- js/cargarDatos.js
+++ js/cargarDatos.js
@@ -40,3 +40,75 @@ const cargarIngredientes = async () => {
 };
 
 cargarIngredientes();
+
+const cargarMasas = async () => {
+  //cargamos la lista de masas del servidor
+  const pizzas = await enviarRequest(
+    "GET",
+    "../server/pizzas.json"
+  );
+  const listaMasa =pizzas.masas;
+  const masaNode = document.getElementById("masa");
+  listaMasa.forEach((masas) => {
+    
+    const divWrapper = document.createElement("div");
+    divWrapper.className = "inline-column";
+    masaNode.appendChild(divWrapper);
+
+    //generamos un id (el nombre del tipo de masa sin espacios)
+    const idMas = `mas-${masas}`;
+    //creamos los radiobuttons
+    const rdio = document.createElement("input");
+    rdio.setAttribute("type", "radio");
+    rdio.setAttribute("id", idMas);
+    rdio.setAttribute("name", "masas");
+    rdio.setAttribute("value", masas);
+    //situamos los radiobuttons en el documento
+    divWrapper.appendChild(rdio);
+
+    //creamos la label
+    const label = document.createElement("label");
+    label.setAttribute("for", idMas);
+    label.textContent = masas;
+    //situamos la label en el documento
+    divWrapper.appendChild(label);
+  });
+};
+
+cargarMasas();
+
+const cargarTamanios = async () => {
+  //cargamos la lista de tamaños del servidor
+  const pizzas = await enviarRequest(
+    "GET",
+    "../server/pizzas.json"
+  ); 
+  const listaTamanios = pizzas.tamanios;
+  const tamaniosNode = document.getElementById("tamanio");
+  listaTamanios.forEach((tamanios) => {
+    
+    const divWrapper = document.createElement("div");
+    divWrapper.className = "inline-column";
+    tamaniosNode.appendChild(divWrapper);
+
+    //generamos un id (el tamaño sin espacios)
+    const idTam = `tam-${tamanios.nombre}`;
+    //creamos los radiobuttons
+    const rdio = document.createElement("input");
+    rdio.setAttribute("type", "radio");
+    rdio.setAttribute("id", idTam);
+    rdio.setAttribute("name", "tamanios");
+    rdio.setAttribute("value", tamanios);
+    //situamos los radiobuttons en el documento
+    divWrapper.appendChild(rdio);
+
+    //creamos la label
+    const label = document.createElement("label");
+    label.setAttribute("for", idTam);
+    label.textContent = tamanios.nombre;
+    //situamos la label en el documento
+    divWrapper.appendChild(label);
+  });
+};
+
+cargarTamanios();
